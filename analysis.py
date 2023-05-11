87a5575e8a64b7d3abd1d87924a22c7792fe09fc
Rebecca Hannah Quinn <rebeccahannahquinn@gmail.com>
2023-05-11 19:43:22 +0100

    editing parameters from work done in analysistestfile

--- analysis.py
+++ analysis.py
@@ -6,9 +6,7 @@ import seaborn as sns
 #reading in the CSV file - acquired from:
 # Read in CSV file
 data = pd.read_csv("iris.csv")
-
 print(data.head())
-#prints off the top 5 rows of the data and then gives us the shape (number of rows and columns of the data file)
 #Result:
 #   sepal_length  sepal_width  petal_length  petal_width species
 #0           5.1          3.5           1.4          0.2  setosa
@@ -17,30 +15,32 @@ print(data.head())
 #3           4.6          3.1           1.5          0.2  setosa
 #4           5.0          3.6           1.4          0.2  setosa
 
+cols = ["sepal_length", "sepal_width", "petal_width", "petal_length"]
+#renameing columns with dictionary to cols to save over dataframe
+data.rename(columns = {cols[0]:0, cols[1]:1, cols[2]:2, cols[3]:3}, inplace=True)
+#like data.head shows every 50th row or the first row of each species data
+data.loc[::50]
 
 print(data.shape)
 #Result is 150 rows and 5 columns (does not include the row numbers column)
 #(150, 5)
 
-df = pd.DataFrame(data)
-data.describe()
-
-
-#Results:
-#       sepal_length  sepal_width  petal_length  petal_width
-#count    150.000000   150.000000    150.000000   150.000000
-#mean       5.843333     3.054000      3.758667     1.198667
-#std        0.828066     0.433594      1.764420     0.763161
-#min        4.300000     2.000000      1.000000     0.100000
-#25%        5.100000     2.800000      1.600000     0.300000
-#50%        5.800000     3.000000      4.350000     1.300000
-#75%        6.400000     3.300000      5.100000     1.800000
-#max        7.900000     4.400000      6.900000     2.500000
+print(data.describe())
+#                0           1           3           2
+#count  150.000000  150.000000  150.000000  150.000000
+#mean     5.843333    3.054000    3.758667    1.198667
+#std      0.828066    0.433594    1.764420    0.763161
+#min      4.300000    2.000000    1.000000    0.100000
+#25%      5.100000    2.800000    1.600000    0.300000
+#50%      5.800000    3.000000    4.350000    1.300000
+#75%      6.400000    3.300000    5.100000    1.800000
+#max      7.900000    4.400000    6.900000    2.500000
+print(data.species.value_counts())
 
 
 
 #using .info we can print the data types to avoid null data etc
-print(df.info())
+print(data.info())
 #Results:
 #RangeIndex: 150 entries, 0 to 149
 #Data columns (total 5 columns):
@@ -55,7 +55,7 @@ print(df.info())
 #memory usage: 6.0+ KB
 
 
-print(df.isnull().sum())
+print(data.isnull().sum())
 #Results:
 #sepal_length    0
 #sepal_width     0
@@ -64,7 +64,7 @@ print(df.isnull().sum())
 #species         0
 #dtype: int64
 
-print(df.value_counts("species"))
+print(data.value_counts("species"))
 #Results:
 #species
 #setosa        50
@@ -80,7 +80,7 @@ print(df.value_counts("species"))
 #pair1 petal_length v petal_width
 plt.gcf().set_size_inches(10, 8)
 #sets the size of image in inches
-sns.scatterplot(x="petal_width", y="petal_length", hue="species", data=df)
+sns.scatterplot(x="petal_width", y="petal_length", hue="species", data=data)
 plt.legend(bbox_to_anchor=(1, 1), loc=2)
 plt.savefig("Petal Width V Petal Length Scatterplot", dpi=200)
 #saves the figure - saved with particular dpi(dots per inch = clarity of image) to fit all in image file
@@ -88,12 +88,12 @@ plt.savefig("Petal Width V Petal Length Scatterplot", dpi=200)
 plt.close()
 #closes the file/image so duplicates do not form in charts and in legend
 #https://stackoverflow.com/questions/36018681/stop-seaborn-plotting-multiple-figures-on-top-of-one-another
-######to stop duplicates come back to this!!!!######
+
 #-----
 
 #pair2 sepal_length v sepal_width
 plt.gcf().set_size_inches(10, 8)
-sns.scatterplot(x="sepal_width", y="sepal_length", hue="species", data=df)
+sns.scatterplot(x="sepal_width", y="sepal_length", hue="species", data=data)
 plt.legend(bbox_to_anchor=(1, 1), loc=2)
 plt.savefig("Sepal Width V Sepal Length Scatterplot", dpi=200)
 plt.close()
@@ -103,7 +103,7 @@ plt.close()
 
 #pair3 sepal_length v petal_length
 plt.gcf().set_size_inches(10, 8)
-sns.scatterplot(x="petal_length", y="sepal_length", hue="species", data=df)
+sns.scatterplot(x="petal_length", y="sepal_length", hue="species", data=data)
 plt.legend(bbox_to_anchor=(1, 1), loc=2)
 plt.savefig("Sepal Length V Petal Length Scatterplot", dpi=200)
 plt.close()
@@ -112,7 +112,7 @@ plt.close()
 
 #pair4 sepal_width v petal_width
 plt.gcf().set_size_inches(10, 8)
-sns.scatterplot(x="petal_width", y="sepal_width", hue="species", data=df)
+sns.scatterplot(x="petal_width", y="sepal_width", hue="species", data=data)
 plt.legend(bbox_to_anchor=(1, 1), loc=2)
 plt.savefig("Sepal Width V Petal Width Scatterplot", dpi=200)
 plt.close()
@@ -121,7 +121,7 @@ plt.close()
 
 #pair5 Sepal Length V Petal Width
 plt.gcf().set_size_inches(10, 8)
-sns.scatterplot(x="petal_width", y="sepal_length", hue="species", data=df)
+sns.scatterplot(x="petal_width", y="sepal_length", hue="species", data=data)
 plt.legend(bbox_to_anchor=(1, 1), loc=2)
 plt.savefig("Sepal Length V Petal Width Scatterplot", dpi=200)
 plt.close()
@@ -130,7 +130,7 @@ plt.close()
 
 #pair6 Sepal Width V Petal Length
 plt.gcf().set_size_inches(10, 8)
-sns.scatterplot(x="sepal_width", y="petal_length", hue="species", data=df)
+sns.scatterplot(x="sepal_width", y="petal_length", hue="species", data=data)
 plt.legend(bbox_to_anchor=(1, 1), loc=2)
 
 plt.savefig("Sepal Width V Petal Length Scatterplot", dpi=200)
@@ -159,9 +159,6 @@ plt.savefig("Histograms")
 #-----
 
 plt.gcf().set_size_inches(14, 10)
-sns.pairplot(df, hue="species")
+sns.pairplot(data, hue="species")
 plt.legend(bbox_to_anchor=(1, 1), loc=2)
 plt.savefig("Overview of Species Comparison")
-
-plt.clf() 
-del df
--- analysistest.py
+++ analysistest.py
@@ -47,3 +47,4 @@ plt.savefig("scatterplot.png")
 
 
 
+
